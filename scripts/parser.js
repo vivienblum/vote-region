46da1bd32250ae428fd916d72d44b860704cfaa8
vivien.blum <vivien.blum@free.fr>
2018-05-11 10:04:45 -0400

    Parser works

--- scripts/parser.js
+++ scripts/parser.js
@@ -2,8 +2,12 @@ function getSalariesByGeo() {
   const data = []
   Papa.parse("data/salaire-net-horaire-moyen-selon-la-categorie-socioprofessionnelle-le-sexe-et-lag.csv", {
     download: true,
+    header: true,
     step: function(row) {
-      data[row.data[0][0]] = row.data[0][2];
+      const index = parseInt(row.data[0][`CODGEO`]);
+      if (!isNaN(index)) {
+        data[index] = row.data[0][`SNHM14`];
+      }
     },
     complete: function() {
       console.log("Finish");
